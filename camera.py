940dcaab8b70b3f900bdea59fa24edc66c7c554e
ce319018 <103323554+ce319018@users.noreply.github.com>
2022-04-14 08:10:22 +0700

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,15 +1,13 @@
 from picamera import PiCamera
 import time
-
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.vflip = True
-
-camera.start_preview()
-time.sleep(5)
-
-camera.stop_preview()
-
-camera.capture("test.jpg")
-
-quit()
+counter = 1
+while(1==1):
+    camera.start_preview()
+    time.sleep(2)
+    camera.capture("Data/foto"+str(counter)+".jpg")
+    camera.stop_preview()
+    counter+=1
+    time.sleep(3600)
